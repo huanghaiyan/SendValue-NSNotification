eb5434cd3f24b2c22519c4afdbd07faad21e4ab1
huanghaiyan <1531398577@qq.com>
2015-11-08 11:14:07 +0800

    commit

---
+++ SendValue-NSNotification/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SendValue-NSNotification
+//
+//  Created by 黄海燕 on 15/11/8.
+//  Copyright © 2015年 Haiyan Huang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
